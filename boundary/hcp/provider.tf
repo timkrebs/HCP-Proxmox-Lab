aed7c2a0241b8cc243a09820de6507a2646cbef3
Tim Krebs <tim.krebs@hashicorp.com>
2025-08-05 17:18:34 +0200

    Update random provider configuration in Terraform to specify version 3.5.1 for improved compatibility.

--- boundary/hcp/provider.tf
+++ boundary/hcp/provider.tf
@@ -2,4 +2,7 @@ provider "hcp" {
   client_id     = var.service-principal-key-client-id
   client_secret = var.service-principal-key-client-secret
 }
-provider random {}
+provider random {
+    version = "~> 3.5.1"
+
+}
